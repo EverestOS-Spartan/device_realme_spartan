4d6a799b90bc171f236c275af7bd62a58a6d254e
vjspranav <pranavasri@live.in>
2024-04-27 22:26:34 +0200

    spartan: Add setup wizard overlays

--- device.mk
+++ device.mk
@@ -344,6 +344,7 @@ PRODUCT_PACKAGES += \
     OPlusSettingsProviderResTarget \
     OPlusSettingsResTarget \
     OPlusSystemUIResTarget \
+    PixelSetupWizardStringsOverlay \
     WifiResTarget
 
 # Oplus Camera
