6dd05d3053370361fd5728f894d75ee66b3fc82a
ryukftw <workforbijoy9@gmail.com>
2024-09-11 03:24:44 +0000

    spartan: adapt to Project Blaze

--- blaze_spartan.mk
+++ blaze_spartan.mk
@@ -11,30 +11,39 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spartan device
 $(call inherit-product, device/realme/spartan/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Projext-Blaze stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
-# Device Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Extras
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
-
-PRODUCT_NAME := aosp_spartan
+PRODUCT_NAME := blaze_spartan
 PRODUCT_DEVICE := spartan
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX3371
 
-PRODUCT_SYSTEM_NAME := RMX3371
-PRODUCT_SYSTEM_DEVICE := RE54E4L1
-
 # Boot Animation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Blaze Maintainer Stuff
+BLAZE_BUILD_TYPE := UNOFFICIAL
+BLAZE_MAINTAINER := RYUK
+
+# Blaze specific Flags
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# udfps Flags
+TARGET_HAS_UDFPS := true
+TARGET_UDFPS_ANIMATIONS := true
+
+# Gapps
+WITH_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+
+PRODUCT_SYSTEM_NAME := RMX3371
+PRODUCT_SYSTEM_DEVICE := RE54E4L1
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
@@ -44,4 +53,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := realme/RMX3371/RE54E4L1:14/UP1A.230620.001/S.14bef93_5a18-26df2:user/release-keys
-
